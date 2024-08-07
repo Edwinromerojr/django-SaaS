51895011c28722c59141117d819f10b415e78239
unknown <edwinrmrjr@gmail.com>
2024-08-07 11:14:51 +0800

    Adding links

--- link/views.py
+++ link/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import CategoryForm
+from .models import Category
+from .forms import CategoryForm, LinkForm
 
 # Create your views here.
 @login_required
@@ -10,12 +11,30 @@ def create_category(request):
         form = CategoryForm(request.POST)
 
         if form.is_valid():
-            link = form.save(commit=False)
-            link.created_by = request.user
-            link.save()
+            category = form.save(commit=False)
+            category.created_by = request.user
+            category.save()
 
             return redirect('/')
     else:
         form = CategoryForm()
 
     return render(request, 'link/create_category.html', {'form': form})
+
+
+@login_required
+def create_link(request):
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+
+        if form.is_valid():
+            link = form.save(commit=False)
+            link.created_by = request.user
+            link.save()
+
+            return redirect('/')
+    else:
+        form = LinkForm()
+        form.fields['category'].queryset = Category.objects.filter(created_by=request.user)
+
+    return render(request, 'link/create_link.html', {'form': form})
