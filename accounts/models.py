e6cd1eba8aae3e3edbc19479bd3ddc7007371f37
unknown <edwinrmrjr@gmail.com>
2024-08-08 13:48:56 +0800

    Limits/Plans

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,16 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class Plan(models.Model):
+    name = models.CharField(max_length=255)
+    max_num_links = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
 class User(AbstractUser):
+    plan = models.ForeignKey(Plan, related_name='users', default=1, on_delete=models.CASCADE)
     pass
